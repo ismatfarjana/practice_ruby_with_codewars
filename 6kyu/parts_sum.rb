807a840bb0c2114a07823cbdc11361eddfb3a690
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-03-19 22:50:52 +1100

    solution 2

--- 6kyu/parts_sum.rb
+++ 6kyu/parts_sum.rb
@@ -1,15 +1,25 @@
-def parts_sums(ls)
-    sums = []
-    number_of_items = ls.count
-    number_of_items.times do 
-      sums << ls
-    end
-    p sums
-    new_array = sums.map.with_index { |arr, i| arr.drop(i) }
-     total = new_array.map {|item| item.sum}
-     p total.push(0)
+# def parts_sums(ls)
+#     sums = []
+#     number_of_items = ls.count
+#     number_of_items.times do 
+#       sums << ls
+#     end
+#     p sums
+#     new_array = sums.map.with_index { |arr, i| arr.drop(i) }
+#      total = new_array.map {|item| item.sum}
+#      p total.push(0)
      
-end
+# end
+
+def parts_sums(ls)
+      sums = []
+      
+      (ls.size+1).times do |index|
+            sums<<ls.drop(index).sum
+      end
+      p sums
+       
+    end
 
 
 
