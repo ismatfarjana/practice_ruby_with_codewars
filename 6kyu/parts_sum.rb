a9eee8cb3a56852376f8d9e0328630c535ddef81
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-03-20 22:51:58 +1100

    solution commented

--- 6kyu/parts_sum.rb
+++ 6kyu/parts_sum.rb
@@ -23,4 +23,13 @@ def parts_sums(ls)
 
 
 
+#   def parts_sums(ls)
+#      sum_of_first_item = ls.sum
+#      total_sum = [sum_of_first_item]
+#      ls.each do |number|
+#       total_sum << sum_of_first_item - number
+#      end
+#      p total_sum
+#   end
+
 parts_sums([0, 1, 3, 6, 10])
